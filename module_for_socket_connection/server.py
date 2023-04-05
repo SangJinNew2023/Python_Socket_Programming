80cfb0a7dcee6228eb15456d55ff8ef10f593801
Sang jin An <photoan79@naver.com>
2023-04-05 03:32:03 -0700

    moduel for socket

--- module_for_socket_connection/server.py
+++ module_for_socket_connection/server.py
@@ -1,18 +1,17 @@
-class TCPServer:
-    def __init__(self, port): #소켓 생성 및 바인딩 후 대기
-        import socket
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.bind(('', port))
-        self.sock.listen(5)
+import MyTCPServer as ms
 
-    def Accept(self): #연결 허용 후 client 소켓 및 주소 반환
-        self.c_sock, self.c_addr = self.sock.accept()
-        return self.c_sock, self.c_addr
+server = ms.TCPServer(2500)
+print("Waiting for connection")
+while True:
+    if not server.connected:
+        server.accept()
+    else:
+        msg = server.receive()
+        if msg:
+            print('receieved message:', msg)
+            server.send(msg)
+        else:
+            print("Disconnected")
+            break
+server.disconnect()
 
-if __name__=='__main__':
-    sock = TCPServer(2500)
-    c.addr = sock.Accept()
-    print('received message:', c.recv(1024).decode())
-    msg = "Hello Client"
-    c.send(msg.encode())
-    c.close
